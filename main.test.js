5f09448ce1e2ff7a5b9329b0df3ee4c3c78b5708
lowhan <b022010033@student.utem.edu.my>
2022-05-27 16:49:33 +0800

    Lab3 - OpenLab - testupdate

--- main.test.js
+++ main.test.js
@@ -1,27 +1,82 @@
 const supertest = require('supertest');
 const request = supertest('http://localhost:3000'); //test on localhost with port 3000
 jest.setTimeout(30000);
-
+ 
 //sample data  
-const sample = {
-	username : "user1",					//to access other sample data = change number
-	password : "passwordfromuser1",
-	phone : "011-111111111",
+const newsample = {						// new sample for register success, login success, delete success
+	username : "usertest1",				// make sure this data is not exist in your mongodb 
+	password : "passwordfromusertest1",
+	phone : "011-1111111",
 	role : "user",
 }
 
+const updatesample = {					// update sample for update success
+	username : "usertest1",					
+	password : "passwordfromusertest1",
+	phone : "999",
+	role : "admin",
+}
+
+const badusersample = {					// badusersample for login fail (invalid username)
+	username : "baduser",					
+	password : "passwordfromusertest1",
+	phone : "011-1111111",
+	role : "user",
+}
+
+const badpasswordsample = {				// badpsample for register fail, login fail (invalid password), update fail, delete fail 
+	username : "usertest1",					
+	password : "badpassword",
+	phone : "011-1111111",
+	role : "user",
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+// Flow of the test :  																				//						   
+// Start > Register success (newsample) > Register fail (badpsample) > Login success (newsample) >  //
+// Login fail (badusersample) > update success (updatesample) > update fail (badpsample) >          //	
+// delete fail (badpsample) > delete success (newsample) > End           							//
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+
 describe('Express Route Test', function () {
 	////////////////////////////////////////////////////////////////////////
-	//Testing
-	// it('should return Welcome to OUR page !', async () => {
-	// 	return request.get('/test')
-	// 		.expect(200)
-	// 		.expect('Content-Type', /text/)
-	// 		.then(res => {
-	// 			expect(res.text).toBe('testing... you are good for now');
-	// 		});
-	// })
+	// Testing
+	it('should return Welcome to OUR page !', async () => {
+		return request.get('/test')
+			.expect(200)
+			.expect('Content-Type', /text/)
+			.then(res => {
+				expect(res.text).toBe('testing... you are good for now');
+			});
+	})
 	
+	///////////////////////////////////////////////////////////////////////
+	// post - create
+
+	// Expected to success
+	it('User Registration - success', async () => {
+		return request
+			.post('/user/register')
+			.send(newsample) //req.body for register
+			.expect(200)
+			.expect('Content-Type', /text/)
+			.then(res => {
+				expect(res.text).toBe("creation success");
+			});
+	});
+
+	// Expected to fail
+	it('User Registration - fail', async () => {
+		return request
+			.post('/user/register')
+			.send(badpasswordsample) //req.body for register
+			.expect(400)
+			.expect('Content-Type', /text/)
+			.then(res => {
+				expect(res.text).toBe("creation fail");
+			});
+	});
+
 	///////////////////////////////////////////////////////////////////////
 	// post - read 
  
@@ -29,7 +84,7 @@ describe('Express Route Test', function () {
 	it('User login - success', async () => {
 		return request
 			.post('/user/login')
-			.send(sample) //req.body
+			.send(newsample) //req.body
 			.expect('Content-Type', /json/)
 			.expect(200).then(response => {
 				expect(response.body).toEqual(
@@ -43,96 +98,71 @@ describe('Express Route Test', function () {
 			});
 	});
 
-	// // Expected to fail
-	// it('User login - fail', async () => {
-	// 	return request
-	// 		.post('/user/login')
-	// 		.send(sample)
-	// 		.expect(400)
-	// 		.expect('Content-Type', /text/)
-	// 		.then(res => {
-	// 			expect(res.text).toBe('login fail');
-	// 		});
-	// })
-
-	///////////////////////////////////////////////////////////////////////
-	// post - create
-
-	// // Expected to success
-	// it('User Registration - success', async () => {
-	// 	return request
-	// 		.post('/user/register')
-	// 		.send(sample) //req.body for register
-	// 		.expect(200)
-	// 		.expect('Content-Type', /text/)
-	// 		.then(res => {
-	// 			expect(res.text).toBe("creation success");
-	// 		});
-	// });
-
-	// // Expected to fail
-	// it('User Registration - fail', async () => {
-	// 	return request
-	// 		.post('/user/register')
-	// 		.send(sample) //req.body for register
-	// 		.expect(400)
-	// 		.expect('Content-Type', /text/)
-	// 		.then(res => {
-	// 			expect(res.text).toBe("creation fail");
-	// 		});
-	// });
-
-	///////////////////////////////////////////////////////////////////////
-	// delete
-
- 	// // Expected to success
-	// it('Delete username - success', async () => {
-	// 	return request
-	// 		.delete('/user/delete')
-	// 		.send(sample) //req.body for register
-	// 		.expect(200)
-	// 		.expect('Content-Type', /text/)
-	// 		.then(res => {
-	// 			expect(res.text).toBe('deletion success');
-	// 		});
-	// });
-
-	// // Expected to fail
-	// it('Delete username - fail', async () => {
-	// 	return request
-	// 		.delete('/user/delete')
-	// 		.send(sample) //req.body for register
-	// 		.expect(400)
-	// 		.expect('Content-Type', /text/)
-	// 		.then(res => {
-	// 			expect(res.text).toBe('deletion fail');
-	// 		});
-	// });
+	// Expected to fail
+	it('User login - fail', async () => {
+		return request
+			.post('/user/login')
+			.send(badusersample)
+			.expect(400)
+			.expect('Content-Type', /text/)
+			.then(res => {
+				expect(res.text).toBe('login fail');
+			});
+	})
 
 	///////////////////////////////////////////////////////////////////////
 	// patch - update
 
-	// // Expected to success
-	// it('Update info - success', async () => {
-	// 	return request
-	// 		.patch('/user/update')
-	// 		.send(sample) //req.body for register
-	// 		.expect(200)
-	// 		.expect('Content-Type', /text/)
-	// 		.then(res => {
-	// 			expect(res.text).toBe('update success');
-	// 		});
-	// });
+	// Expected to success
+	it('Update info - success', async () => {
+		return request
+			.patch('/user/update')
+			.send(updatesample) //req.body for register
+			.expect(200)
+			.expect('Content-Type', /text/)
+			.then(res => {
+				expect(res.text).toBe('update success');
+			});
+	});
 
-	// // Expected to fail
-	// it('Update info - fail', async () => {
-	// 	return request
-	// 		.patch('/user/update')
-	// 		.send(sample) //req.body for register
-	// 		.expect(400)
-	// 		.expect('Content-Type', /text/)
-	// 		.then(res => {
-	// 			expect(res.text).toBe('update fail');
-	// 		});
-	// });
+	// Expected to fail
+	it('Update info - fail', async () => {
+		return request
+			.patch('/user/update')
+			.send(badpasswordsample) //req.body for register
+			.expect(400)
+			.expect('Content-Type', /text/)
+			.then(res => {
+				expect(res.text).toBe('update fail');
+			});
+	});
+
+	///////////////////////////////////////////////////////////////////////
+	// delete
+
+	// Expected to fail
+	it('Delete username - fail', async () => {
+		return request
+			.delete('/user/delete')
+			.send(badpasswordsample) //req.body for register
+			.expect(400)
+			.expect('Content-Type', /text/)
+			.then(res => {
+				expect(res.text).toBe('deletion fail');
+			});
+	});
+
+	// Expected to success
+	it('Delete username - success', async () => {
+		return request
+			.delete('/user/delete')
+			.send(newsample) //req.body for register
+			.expect(200)
+			.expect('Content-Type', /text/)
+			.then(res => {
+				expect(res.text).toBe('deletion success');
+			});
+	});
+
+	///////////////////////////////////////////////////////////////////////
 });
--- user.test.js
+++ user.test.js
@@ -2,12 +2,40 @@ const MongoClient = require("mongodb").MongoClient;
 const User = require("./user");
 
 //sample data  
-const sample = {
-	username : "user1",					//to access other sample data = change number
-	password : "passwordfromuser1",
+const newsample = {						// new sample for register success, login success, delete success
+	username : "usertest2",				// make sure this data is not exist in your mongodb 
+	password : "passwordfromusertest2",
 	phone : "011-1111111",
 	role : "user",
 }
+
+const updatesample = {					// update sample for update success
+	username : "usertest2",					
+	password : "passwordfromusertest2",
+	phone : "999",
+	role : "admin",
+}
+
+const badusersample = {					// badusersample for login fail (invalid username)
+	username : "baduser",					
+	password : "passwordfromusertest2",
+	phone : "011-1111111",
+	role : "user",
+}
+
+const badpasswordsample = {				// badpsample for register fail, login fail (invalid password), update fail, delete fail 
+	username : "usertest2",					
+	password : "badpassword",
+	phone : "011-1111111",
+	role : "user",
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+// Flow of the test :  																				//						   
+// Start > Register success (newsample) > Register fail (badpsample) > Login success (newsample) >  //
+// Login fail (badusersample) > Login fail (badpsample) > update success (updatesample) >           //
+// update fail (badpsample) > delete fail (badpsample) > delete success (newsample) > End           //
+//////////////////////////////////////////////////////////////////////////////////////////////////////
 
 describe("User Account Management", () => {
 	let client;
@@ -25,74 +53,75 @@ describe("User Account Management", () => {
 	///////////////////////////////////////////////////////////////
 					// create - registration //
 
-	// test("User Registration - success - new username", async () => {
-	// 	const res = await User.register(sample);
-	// 	expect(res).toBe("creation success");
-	// })
+	test("User Registration - success - new username", async () => {
+		const res = await User.register(newsample);
+		expect(res).toBe("creation success");
+	})
 
-	// test("User Registration - fail - duplicate username", async () => {
-	// 	const res = await User.register(sample);
-	// 	expect(res).toBe("creation fail");
-	// })
+	test("User Registration - fail - duplicate username", async () => {
+		const res = await User.register(badpasswordsample);
+		expect(res).toBe("creation fail");
+	})
 
 	///////////////////////////////////////////////////////////////
 					// read - login & return data  //
 
-	// test("User login - invalid username", async () => {
-	// 	const res = await User.login(sample);
-	// 	expect(res).toBe("invalid username");
-	// })
-
-	// test("User login - invalid password", async () => {
-	// 	const res = await User.login(sample);
-	// 	expect(res).toBe("invalid password");
-	// })
-
 	test("User login - success", async () => {
-		const res = await User.login(sample);
-		expect(res.username).toBe(sample.username);
+		const res = await User.login(newsample);
+		expect(res.username).toBe(newsample.username);
 		expect(res.password).toEqual(expect.any(String)); //it already encrypt so... test can't expect it
 	})
-	
-	///////////////////////////////////////////////////////////////
-					// delete - match & delete //
 
-	// test("Delete username - success", async () => {
-	// 	const res = await User.delete(sample);
-	// 	expect(res).toBe("deletion success");
-	// })
+	test("User login - invalid username", async () => {
+		const res = await User.login(badusersample);
+		expect(res).toBe("invalid username");
+	})
 
-	// test("Delete username - fail", async () => {
-	// 	const res = await User.delete(sample);
-	// 	try 
-	// 	{
-	// 		expect(res).toBe("deletion fail");     //wrong password
-	// 	}
-	// 	catch
-	// 	{
-	// 		expect(res).toBe("invalid username");  //no user found
-	// 	}
-	// })
+	test("User login - invalid password", async () => {
+		const res = await User.login(badpasswordsample);
+		expect(res).toBe("invalid password");
+	})
 
 	///////////////////////////////////////////////////////////////
 					// update - match & update //
 
-	// test("Update info - success", async () => {
-	// 	const res = await User.update(sample);
-	// 	expect(res).toBe("update success");
-	// })
+	test("Update info - success", async () => {
+		const res = await User.update(updatesample);
+		expect(res).toBe("update success");
+	})
 
-	// test("Update info - fail", async () => {
-	// 	const res = await User.update(sample);
-	// 	try 
-	// 	{
-	// 		expect(res).toBe("update fail");     //wrong password
-	// 	}
-	// 	catch
-	// 	{
-	// 		expect(res).toBe("invalid username");  //no user found
-	// 	}
-	// })
+	test("Update info - fail", async () => {
+		const res = await User.update(badpasswordsample);
+		try 
+		{
+			expect(res).toBe("update fail");     //wrong password
+		}
+		catch
+		{
+			expect(res).toBe("invalid username");  //no user found
+		}
+	})
 
 	///////////////////////////////////////////////////////////////
+					// delete - match & delete //
+
+	test("Delete username - fail", async () => {
+		const res = await User.delete(badpasswordsample);
+		try 
+		{
+			expect(res).toBe("deletion fail");     //wrong password
+		}
+		catch
+		{
+			expect(res).toBe("invalid username");  //no user found
+		}
+	})
+	
+	test("Delete username - success", async () => {
+		const res = await User.delete(newsample);
+		expect(res).toBe("deletion success");
+	})
+
+	///////////////////////////////////////////////////////////////
+					
 });
